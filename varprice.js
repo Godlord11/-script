9ba8ce0c588358fb1380344c3290847e0456bfa8
Devansh Panchal <96012309+Godlord11@users.noreply.github.com>
2023-03-04 10:50:00 +0530

    Update varprice.js

--- varprice.js
+++ varprice.js
@@ -1,58 +1,40 @@
+// THE BELOW 4 ELEMENTS ARE FOR LAZADA PRICE //
+const retail1 = document.getElementById("retail1"); // 12 months //
+retail1.innerHTML = "S$100.00";
 
-// rosdg = retail price / original price
-var rosdg12 = 500.00;
-var rosdg6 = 500.00;
-var rosdg3 = 500.00;
-var rosdg1 = 500.00;
+const retail2 = document.getElementById("retail2"); // 6 months //
+retail2.innerHTML = "S$100.00";
 
-// rsdg = on lazada price
-var rsdg12 = 500.00;
-var rsdg6 = 500.00;
-var rsdg3 = 500.00;
-var rsdg1 = 500.00;
+const retail3 = document.getElementById("retail3"); // 3 months //
+retail3.innerHTML = "S$100.00";
 
-
+const retail4 = document.getElementById("retail4"); // 1 month //
+retail4.innerHTML = "S$100.00";
 
 
+// THE BELOW 4 ELEMENTS ARE FOR ORIGINAL PRICE //
+const retail5 = document.getElementById("retail5"); // 12 months //
+retail5.innerHTML = "S$100.00";
 
-// rssdg = savings price (auto calculated)
-var rssdg12 = rosdg12 - rsdg12;
-var rssdg6 = rosdg6 - rsdg6;
-var rssdg3 = rosdg3 - rsdg3;
-var rssdg1 = rosdg1 - rsdg1;
+const retail6 = document.getElementById("retail6"); // 6 months //
+retail6.innerHTML = "S$100.00";
+
+const retail7 = document.getElementById("retail7"); // 3 months //
+retail7.innerHTML = "S$100.00";
+
+const retail8 = document.getElementById("retail8"); // 1 month //
+retail8.innerHTML = "S$100.00";
 
 
+// THE BELOW 4 ELEMENTS ARE FOR SAVINGS PRICE //
+const retail9 = document.getElementById("retail9"); // 12 months //
+retail9.innerHTML = "S$100.00";
 
+const retail10 = document.getElementById("retail10"); // 6 months //
+retail10.innerHTML = "S$100.00";
 
+const retail11 = document.getElementById("retail11"); // 3 months //
+retail11.innerHTML = "S$100.00";
 
-
-// DO NOT TOUCH //
-let osdg12 = "S$" + rosdg12  ;
-let osdg6 = "S$" + rosdg12 ;
-let osdg3 = "S$" + rosdg12 ;
-let osdg1 = "S$" + rosdg12 ;
-
-let sdg12 = "S$" + rsdg12;
-let sdg6 = "S$" + rsdg12;
-let sdg3 = "S$" + rsdg12;
-let sdg1 = "S$" + rsdg12;
-
-let ssdg12 = "S$" + rssdg12;
-let ssdg6 = "S$" + rssdg12;
-let ssdg3 = "S$" + rssdg12;
-let ssdg1 = "S$" + rssdg12;
-
-document.getElementById("osdg1").innerHTML = osdg1;
-document.getElementById("osdg3").innerHTML = osdg3;
-document.getElementById("osdg6").innerHTML = osdg6;
-document.getElementById("osdg12").innerHTML = osdg12;
-
-document.getElementById("sdg1").innerHTML = sdg1;
-document.getElementById("sdg3").innerHTML = sdg3;
-document.getElementById("sdg6").innerHTML = sdg6;
-document.getElementById("sdg12").innerHTML = sdg12;
-
-document.getElementById("ssdg1").innerHTML = ssdg1;
-document.getElementById("ssdg3").innerHTML = ssdg3;
-document.getElementById("ssdg6").innerHTML = ssdg6;
-document.getElementById("ssdg12").innerHTML = ssdg12;
+const retail12 = document.getElementById("retail12"); // 1 month //
+retail12.innerHTML = "S$100.00";
