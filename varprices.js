ddffe13c576c88235c62611ac690aebbd7da5aaf
Devansh Panchal <96012309+Godlord11@users.noreply.github.com>
2023-03-10 12:57:31 +0530

    Update varprices.js

--- varprices.js
+++ varprices.js
@@ -1,20 +1,20 @@
 // THE BELOW 4 ELEMENTS ARE FOR LAZADA PRICE //
 const retail1 = document.getElementById("retail1"); // 12 months //
-retail1.innerHTML = "S$551.9";
+retail1.innerHTML = "S$551.90";
 
 const retail2 = document.getElementById("retail2"); // 6 months //
-retail2.innerHTML = "S$354";
+retail2.innerHTML = "S$354.00";
 
 const retail3 = document.getElementById("retail3"); // 3 months //
-retail3.innerHTML = "S$183";
+retail3.innerHTML = "S$183.00";
 
 const retail4 = document.getElementById("retail4"); // 1 month //
-retail4.innerHTML = "S$63";
+retail4.innerHTML = "S$63.00";
 
 
 // THE BELOW 4 ELEMENTS ARE FOR ORIGINAL PRICE //
 const retail5 = document.getElementById("retail5"); // 12 months //
-retail5.innerHTML = "S$991.2";
+retail5.innerHTML = "S$991.20";
 
 const retail6 = document.getElementById("retail6"); // 6 months //
 retail6.innerHTML = "S$495.60";
@@ -23,18 +23,18 @@ const retail7 = document.getElementById("retail7"); // 3 months //
 retail7.innerHTML = "S$247.80";
 
 const retail8 = document.getElementById("retail8"); // 1 month //
-retail8.innerHTML = "S$82.6";
+retail8.innerHTML = "S$82.60";
 
 
 // THE BELOW 4 ELEMENTS ARE FOR SAVINGS PRICE //
 const retail9 = document.getElementById("retail9"); // 12 months //
-retail9.innerHTML = "S$439.3";
+retail9.innerHTML = "S$439.30";
 
 const retail10 = document.getElementById("retail10"); // 6 months //
-retail10.innerHTML = "S$141.6";
+retail10.innerHTML = "S$141.60";
 
 const retail11 = document.getElementById("retail11"); // 3 months //
-retail11.innerHTML = "S$64.8";
+retail11.innerHTML = "S$64.80";
 
 const retail12 = document.getElementById("retail12"); // 1 month //
-retail12.innerHTML = "S$19.6";
+retail12.innerHTML = "S$19.60";
