bd5ae55f88774688edcfc4258e320df91f51739c
Kumailt <126945061+Kumailt@users.noreply.github.com>
2023-03-28 14:11:00 +0900

    Update varprices.js

--- varprices.js
+++ varprices.js
@@ -3,13 +3,13 @@ const retail1 = document.getElementById("retail1"); // 12 months //
 retail1.innerHTML = "S$551.90";
 
 const retail2 = document.getElementById("retail2"); // 6 months //
-retail2.innerHTML = "S$354.00";
+retail2.innerHTML = "S$372.00";
 
 const retail3 = document.getElementById("retail3"); // 3 months //
-retail3.innerHTML = "S$183.00";
+retail3.innerHTML = "S$192.00";
 
 const retail4 = document.getElementById("retail4"); // 1 month //
-retail4.innerHTML = "S$63.00";
+retail4.innerHTML = "S$66.20";
 
 
 // THE BELOW 4 ELEMENTS ARE FOR ORIGINAL PRICE //
@@ -31,10 +31,10 @@ const retail9 = document.getElementById("retail9"); // 12 months //
 retail9.innerHTML = "S$439.30";
 
 const retail10 = document.getElementById("retail10"); // 6 months //
-retail10.innerHTML = "S$141.60";
+retail10.innerHTML = "S$123.60";
 
 const retail11 = document.getElementById("retail11"); // 3 months //
-retail11.innerHTML = "S$64.80";
+retail11.innerHTML = "S$55.80";
 
 const retail12 = document.getElementById("retail12"); // 1 month //
-retail12.innerHTML = "S$19.60";
+retail12.innerHTML = "S$16.40";
